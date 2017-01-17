67402cba712bf00596aa803b0b31ceb5f3ca7013
Rose <lain@lain.org.uk>
2017-01-17 02:00:43 +0000

    set up regex input

--- src/bin/rustrip.rs
+++ src/bin/rustrip.rs
@@ -1,4 +1,28 @@
 extern crate rustrip;
+extern crate regex;
+
+use std::env::args;
+use std::io::{stdin, BufRead};
+use regex::Regex;
 
 fn main() {
+  let strings: Vec<String>;
+  let regexes: Vec<Regex>;
+
+  if args().count() < 2 {
+    let stdin = stdin(); {
+      strings = stdin.lock().lines().map(|x| x.unwrap()).collect();
+    }
+  }
+
+  else {
+    strings = args().skip(1).collect();
+  }
+
+  regexes = strings
+    .iter()
+    .map(|regstr| Regex::new(&regstr).expect("Improper regular expression"))
+    .collect();
+
+  println!("{:?}", regexes);
 }
