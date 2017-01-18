c7d9ba8f22d8d20156c2a4c0647aeaa1d5ff2711
Rose <lain@lain.org.uk>
2017-01-18 00:43:37 +0000

    add some generation

--- src/bin/rustrip.rs
+++ src/bin/rustrip.rs
@@ -1,9 +1,24 @@
 extern crate rustrip;
 extern crate regex;
+extern crate rand;
 
 use std::env::args;
 use std::io::{stdin, BufRead};
 use regex::Regex;
+use rand::distributions::{IndependentSample, Range};
+
+fn gen_pass(min: usize, max: usize) -> String {
+  let mut buf = String::new();
+  let mut rng = rand::thread_rng();
+  let ascii_range: Range<u8> = Range::new(33, 127);
+  let length = Range::new(min, max).ind_sample(&mut rng);
+
+  for i in 0..length {
+    buf.push(ascii_range.ind_sample(&mut rng) as char);
+  }
+
+  buf
+}
 
 fn main() {
   let strings: Vec<String>;
@@ -24,5 +39,9 @@ fn main() {
     .map(|regstr| Regex::new(&regstr).expect("Improper regular expression"))
     .collect();
 
-  println!("{:?}", regexes);
+  loop {
+    let pass: &str = &gen_pass(4, 12); {
+      println!("{} -> {:?}", pass, rustrip::encode(pass));
+    }
+  }
 }
